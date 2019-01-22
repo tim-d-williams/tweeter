6c15ccb855e3c0cc7dea6256b108ab5afaee6983
Tim Williams <timwilliams.tx@gmail.com>
2019-01-22 22:48:28 +0000

    target and update counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   console.log('dom has loaded')
+
+
+  $('.new-tweet textarea').on('keyup', function()  {
+    let tweetLength = $(this).val().length
+    // target counter
+    $(this).siblings('.counter').text(140 - tweetLength)
+  })
+
+
+
 });
 
-$('.new-tweet textarea').on('keypress', function()  {
-  // console.log(this.textLength)
-  let charsLeft = 139;
-  console.log(charsLeft -= $(this).val().length)
-
-
-
-});
-
-
-
-// document.getElementsByTagName('textarea').text.textLength
