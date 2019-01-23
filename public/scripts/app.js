446232928dd484ea36f8c6f39d78045ca50d5d50
Tim Williams <timwilliams.tx@gmail.com>
2019-01-23 23:52:37 +0000

    display new tweet at top

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,24 +7,51 @@
 $(document).ready(function() {
 
   function loadTweets () {
-    $.get('http://localhost:8080/tweets')
+    $.get('/tweets')
     .then (tweets => {
-      tweets.forEach (tweet => {
-        let tweetElement = createTweetElement(tweet)
-        renderTweets(tweetElement)
+        renderTweets(tweets)
       })
-    })
+    }
+
+  let createTweetElement = function(tweet) {
+    var $tweet = $("<article>").addClass("tweet")
+
+    //set all header elements to var
+    let header = $('<header>')
+      .append($('<img>')
+        .attr('src', tweet.user.avatars.regular))
+      .append($('<h3>').text(tweet.user.name))
+      .append($('<h4>').text(tweet.user.handle))
+
+      //set body elements
+    let body = $("<div>").addClass("tweet-body")
+      .append($("<p>").text(tweet.content.text))
+
+      //set all footer elements to var
+    let footer = $('<footer>')
+      .append($("<p>").text(Date.now() - tweet.created_at))
+      .append($('<div>').addClass("icons")
+        .append($('<img>')
+          .attr('src', "images/baseline-flag-24px.svg"))
+        .append($('<img>')
+          .attr('src', "images/iconmonstr-retweet-1.svg"))
+        .append($('<img>')
+          .attr('src', "images/baseline-favorite-24px.svg")))
+
+    //append all content to article for display
+      $tweet.append($(header))
+        .append($(body))
+        .append($(footer))
+
+        return $tweet
   }
-loadTweets();
 
-$('#new-tweet').on('submit', function (event) {
-  event.preventDefault();
-  let tweetData = $(this).serialize()
+  $('#new-tweet').on('submit', function (event) {
+    event.preventDefault();
+    let tweetData = $(this).serialize()
 
-  postTweet(tweetData);
-})
-
-
+    postTweet(tweetData);
+    })
 
   function postTweet(tweetData) {
     if ($('textarea').val().length > 140) {
@@ -33,50 +60,19 @@ $('#new-tweet').on('submit', function (event) {
        ($('textarea').val() === "" || null ) {
         alert('Your tweet is empty! You can do better!')
     } else {
-        $.post('http://localhost:8080/tweets', tweetData).then (tweet => {
-        let tweetElement = createTweetElement(tweet)
-        render(tweetElement)
+        $.post('/tweets', tweetData).then (tweet => {
+        loadTweets(tweet)
       })
     }
   }
 
+  function renderTweets(tweets) {
+    $('.tweet').remove()
+    tweets.forEach(tweet => {
+     let tweetData = createTweetElement(tweet);
+     $('#tweet-container').prepend(tweetData)
+    });
+  }
 
-function renderTweets(tweets) {
-  $('#tweet-container').append(tweets)
-  };
-
-let createTweetElement = function(tweet) {
-  var $tweet = $("<article>").addClass("tweet")
-
-  //set all header elements to var
-  let header = $('<header>')
-    .append($('<img>')
-      .attr('src', tweet.user.avatars.regular))
-    .append($('<h3>').text(tweet.user.name))
-    .append($('<h4>').text(tweet.user.handle))
-
-    //set body elements
-  let body = $("<div>").addClass("tweet-body")
-    .append($("<p>").text(tweet.content.text))
-
-    //set all footer elements to var
-  let footer = $('<footer>')
-    .append($("<p>").text(Date.now() - tweet.created_at))
-    .append($('<div>').addClass("icons")
-      .append($('<img>')
-        .attr('src', "images/baseline-flag-24px.svg"))
-      .append($('<img>')
-        .attr('src', "images/iconmonstr-retweet-1.svg"))
-      .append($('<img>')
-        .attr('src', "images/baseline-favorite-24px.svg")))
-
-   //append all content to article for display
-    $tweet.append($(header))
-      .append($(body))
-      .append($(footer))
-
-      $('#tweet-container').append($tweet)
-      return $tweet
-}
-
+loadTweets();
 })
