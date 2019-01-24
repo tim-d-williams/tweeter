36f3919e377599dff17663353a66437d4cb69c4e
Tim Williams <timwilliams.tx@gmail.com>
2019-01-24 21:46:47 +0000

    lint and format code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,29 +9,32 @@ $(document).ready(function() {
   //Attach timeago to DOM
   $("time.timeago").timeago();
 
+  //get all of the tweet data from the server
   function loadTweets () {
     $.get('/tweets')
     .then (tweets => {
-        renderTweets(tweets)
-      })
+        renderTweets(tweets);
+      });
     }
+  loadTweets();
 
+  //dynamically create each tweet article
   let createTweetElement = function(tweet) {
-    var $tweet = $("<article>").addClass("tweet")
+    var $tweet = $("<article>").addClass("tweet");
 
     //set all header elements to var
     let header = $('<header>')
       .append($('<img>')
         .attr('src', tweet.user.avatars.regular))
       .append($('<h3>').text(tweet.user.name))
-      .append($('<h4>').text(tweet.user.handle))
+      .append($('<h4>').text(tweet.user.handle));
 
-      //set body elements
+    //set body elements
     let body = $("<div>").addClass("tweet-body")
-      .append($("<p>").text(tweet.content.text))
+      .append($("<p>").text(tweet.content.text));
 
-      //set all footer elements to var
-      let timeSince = $.timeago(tweet.created_at)
+    //set all footer elements to var
+    let timeSince = $.timeago(tweet.created_at);
     let footer = $('<footer>')
       .append($("<p>").text(timeSince))
       .append($('<div>').addClass("icons")
@@ -40,72 +43,76 @@ $(document).ready(function() {
         .append($('<img>')
           .attr('src', "images/iconmonstr-retweet-1.svg"))
         .append($('<img>')
-          .attr('src', "images/baseline-favorite-24px.svg")))
+          .attr('src', "images/baseline-favorite-24px.svg")));
 
     //append all content to article for display
       $tweet.append($(header))
         .append($(body))
-        .append($(footer))
+        .append($(footer));
 
-        return $tweet
-  }
+        return $tweet;
+  };
 
+  //when tweet is submitted
+    //prevent default posting to url
+    //serialize the data to post
+  //if there is an error displayed,
+    //close the error and clear the html data then validate
+  //if no error is displayed, validate
   $('#new-tweet').on('submit', function (event) {
     event.preventDefault();
-    let tweetData = $(this).serialize()
+    let tweetData = $(this).serialize();
 
     if ($('.error').is(':visible')) {
       ($('.error').slideToggle(200)
         .children().remove()
-          .css( {display: 'none'} ))
+          .css( {display: 'none'} ));
       }
-    postTweet(tweetData);
-  })
 
-  function postTweet(tweetData) {
-
-    if (!$.trim($('textarea').val())) {
-      const errorTooShort = $('.error').prepend($('<img>').attr('src', "images/error.png"))
-        .append($("<p>").text('Your tweet is empty!'));
-
-        $('.error').slideToggle(100, function() {
-          $(this).append(errorTooShort)
-        })
-      }
-      else if ($('textarea').val().length > 140) {
-        const errorTooLong = $('.error').prepend($('<img>').attr('src', "images/error.png"))
-          .append($("<p>").text('Your tweet is too long!'));
+      if (!$.trim($(tweetData))) {
+        const errorTooShort = $('.error').prepend($('<img>').attr('src', "images/error.png"))
+          .append($("<p>").text('Your tweet is empty!'));
 
           $('.error').slideToggle(100, function() {
-            $(this).append(errorTooLong)
-          })
-      }
+            $(this).append(errorTooShort);
+          });
+        }
+        else if ($(tweetData).length > 140) {
+          const errorTooLong = $('.error').prepend($('<img>').attr('src', "images/error.png"))
+            .append($("<p>").text('Your tweet is too long!'));
+
+            $('.error').slideToggle(100, function() {
+              $(this).append(errorTooLong);
+            });
+        }
         else {
-        $.post('/tweets', tweetData).then (tweet => {
-          $('.new-tweet textarea').val('')
-          let counter = $('.new-tweet textarea').siblings('footer').children('.counter')
-          $(counter).text('140');
-          loadTweets(tweet)
+          postTweet(tweetData);
+        }
+  });
 
-        })
-    }
-  }
-
-  function renderTweets(tweets) {
-    $('.tweet').remove()
-    tweets.forEach(tweet => {
-     let tweetData = createTweetElement(tweet);
-     $('#tweet-container').prepend(tweetData)
+  //post date to server, clear text area and reset counter
+  function postTweet(tweetData) {
+    $.post('/tweets', tweetData).then (tweet => {
+      $('.new-tweet textarea').val('');
+      let counter = $('.new-tweet textarea').siblings('footer').children('.counter');
+      $(counter).text('140');
+      loadTweets(tweet);
     });
   }
 
-loadTweets();
+  function renderTweets(tweets) {
+    $('.tweet').remove();
+    tweets.forEach(tweet => {
+     let tweetData = createTweetElement(tweet);
+     $('#tweet-container').prepend(tweetData);
+    });
+  }
 
-$( "#compose" ).click(function() {
-  $( ".new-tweet" ).slideToggle(100, function() {
-    $('textarea').focus()
-    })
-  })
+  //compse button toggles new tweet section
+  $( "#compose" ).click(function() {
+    $( ".new-tweet" ).slideToggle(100, function() {
+      $('textarea').focus();
+      });
+    });
 
-
-})
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
 
   $('.new-tweet textarea').on('keyup', function()  {
-    let tweetLength = $(this).val().length
+    let tweetLength = $(this).val().length;
     let counter = $(this).siblings('footer').children('.counter');
 
     // update counter
-    $(counter).text(140 - tweetLength)
+    $(counter).text(140 - tweetLength);
   if (tweetLength > 140) {
-    $(counter).css({color: 'red'})
-  } else {
-    $(counter).css({color: 'black' })
-  }
-
-})
-
+    $(counter).css({color: 'red'});
+    } else {
+    $(counter).css({color: 'black' });
+   }
+  });
 });
